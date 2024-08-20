0baf0aaa4ba1c2a65cd5da38698f5098b32821c1
mamun-prodhan <mamunpr9@gmail.com>
2024-08-21 05:55:09 +0600

    filters completed

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -39,7 +39,9 @@ const Todo = ({ todo }) => {
         )}
       </div>
 
-      <div className="select-none flex-1 line-through">{todo.text}</div>
+      <div className={`select-none flex-1 ${completed && "line-through"}`}>
+        {todo.text}
+      </div>
 
       <div
         className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-green-500 hover:bg-green-500 ${
